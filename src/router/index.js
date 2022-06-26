a9ffef862735b92035c976056ed82a5450e847ad
羅~東~澤 <995975306@qq.com>
2022-06-26 20:05:41 +0800

    构建登录页面ui结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Login from "../views/login";
 
 Vue.use(VueRouter);
 
-const routes = [
+const publicRoutes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/login",
+    name: "login",
+    component: Login,
   },
 ];
 
 const router = new VueRouter({
-  routes,
+  routes:publicRoutes
 });
 
 export default router;
---
+++ src/views/login/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-container">
+
+    <el-form>
+
+      <!-- title -->
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+
+      <!-- 用户名 -->
+      <el-form-item>
+        <el-input placeholder="username" type="text">
+           <i slot="prefix" class="el-icon-user-solid"></i>
+        </el-input>
+      </el-form-item>
+
+      <!-- 密码 -->
+       <el-form-item>
+        <el-input placeholder="password" type="password">
+           <i slot="prefix" class="el-icon-user-solid"></i>
+           <i slot="suffix" class="el-icon-user-solid"></i>
+        </el-input>
+      </el-form-item>
+
+      <!-- 登录 -->
+      <el-form-item>
+        <el-button type="primary" style="width:100%">登录</el-button>
+      </el-form-item>
+
+    </el-form>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
